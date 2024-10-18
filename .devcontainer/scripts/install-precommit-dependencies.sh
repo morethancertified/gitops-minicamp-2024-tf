f5a9d02a8ef25303a5110bbd625afef7ec6b1dc7
Rod Stewart <rodstewart@gmail.com>
2024-10-18 15:46:54 -0500

    chore: Adds devcontainer, pre-commit, and dependabot (#1)
    * chore: Adds devcontainer and dependabot
    
    * chore:  Updates pre-commit-config.yaml

---
+++ .devcontainer/scripts/install-precommit-dependencies.sh
@@ -0,0 +1,3 @@
+sudo apt update
+sudo apt install -y unzip software-properties-common
+curl -L "$(curl -s https://api.github.com/repos/minamijoyo/tfupdate/releases/latest | grep -o -E -m 1 "https://.+?_linux_amd64.tar.gz")" > tfupdate.tar.gz && tar -xzf tfupdate.tar.gz tfupdate && rm tfupdate.tar.gz && sudo mv tfupdate /usr/bin/
